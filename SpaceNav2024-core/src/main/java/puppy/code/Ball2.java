20a0c1f7694f366b9e8592a519514cb169c0f2ed
Sofia <>
2024-10-31 22:14:42 -0300

    Cambios en pantalla menu para mejorar legibilidad

--- SpaceNav2024-core/src/main/java/puppy/code/Ball2.java
+++ SpaceNav2024-core/src/main/java/puppy/code/Ball2.java
@@ -6,8 +6,6 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Rectangle;
 
-a
-
 public class Ball2 {
 	private int x;
     private int y;
--- SpaceNav2024-core/src/main/java/puppy/code/PantallaMenu.java
+++ SpaceNav2024-core/src/main/java/puppy/code/PantallaMenu.java
@@ -6,75 +6,55 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-
 public class PantallaMenu implements Screen {
 
-	private SpaceNavigation game;
-	private OrthographicCamera camera;
+    private final SpaceNavigation game;
+    private final OrthographicCamera camera;
 
-	public PantallaMenu(SpaceNavigation game) {
-		this.game = game;
-        
-		camera = new OrthographicCamera();
-		camera.setToOrtho(false, 1200, 800);
-	}
+    public PantallaMenu(SpaceNavigation game) {
+        this.game = game;
+        this.camera = new OrthographicCamera();
+        this.camera.setToOrtho(false, 1200, 800);
+    }
 
-	@Override
-	public void render(float delta) {
-		ScreenUtils.clear(0, 0, 0.2f, 1);
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(0, 0, 0.2f, 1);
 
-		camera.update();
-		game.getBatch().setProjectionMatrix(camera.combined);
+        camera.update();
+        game.getBatch().setProjectionMatrix(camera.combined);
 
-		game.getBatch().begin();
-		game.getFont().draw(game.getBatch(), "Bienvenido a Space Navigation !", 140, 400);
-		game.getFont().draw(game.getBatch(), "Pincha en cualquier lado o presiona cualquier tecla para comenzar ...", 100, 300);
-	
-		game.getBatch().end();
+        game.getBatch().begin();
+        game.getFont().draw(game.getBatch(), "Bienvenido a Space Navigation !", 140, 400);
+        game.getFont().draw(game.getBatch(), "Pincha en cualquier lado o presiona cualquier tecla para comenzar ...", 100, 300);
+        game.getBatch().end();
 
-		if (Gdx.input.isTouched() || Gdx.input.isKeyJustPressed(Input.Keys.ANY_KEY)) {
-			Screen ss = new PantallaJuego(game,1,3,0,1,1,10);
-			ss.resize(1200, 800);
-			game.setScreen(ss);
-			dispose();
-		}
-	}
-	
-	
-	@Override
-	public void show() {
-		// TODO Auto-generated method stub
-		
-	}
+        if (Gdx.input.isTouched() || Gdx.input.isKeyJustPressed(Input.Keys.ANY_KEY)) {
+            iniciarJuego();
+        }
+    }
 
-	@Override
-	public void resize(int width, int height) {
-		// TODO Auto-generated method stub
-		
-	}
+    private void iniciarJuego() {
+        PantallaJuego pantallaJuego = new PantallaJuego(game, 1, 3, 0, 1, 1, 10);
+        game.setScreen(pantallaJuego);
+        dispose();
+    }
 
-	@Override
-	public void pause() {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void show() {}
 
-	@Override
-	public void resume() {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void resize(int width, int height) {}
 
-	@Override
-	public void hide() {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void pause() {}
 
-	@Override
-	public void dispose() {
-		// TODO Auto-generated method stub
-		
-	}
-   
+    @Override
+    public void resume() {}
+
+    @Override
+    public void hide() {}
+
+    @Override
+    public void dispose() {}
 }
