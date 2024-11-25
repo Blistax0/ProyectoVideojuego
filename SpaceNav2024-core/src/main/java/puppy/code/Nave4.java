1ea43ec8c3e4038d9805bcc761ae4877a3b64de6
Kamila <>
2024-11-25 01:26:53 -0300

    Cambios en nave4

--- SpaceNav2024-core/src/main/java/puppy/code/Nave4.java
+++ SpaceNav2024-core/src/main/java/puppy/code/Nave4.java
@@ -9,21 +9,26 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
 
 public class Nave4 extends TipoObjeto implements movible{
-    private boolean destruida = false;
+	private float tiempoPowerUp;
+	private float duracionMaxima = 10;
+	
+	private boolean destruida = false;
     private boolean herido = false;
     private int tiempoHeridoMax = 50;
     private int tiempoHerido;
     private Sound sonidoHerido;
     private controlarDisparos Disparo;
+    private StrategyDisparo tipoDisparo;
 
     public Nave4(int x, int y, Texture tx, Sound soundChoque, Texture txBala, Sound soundBala) {
         super(new Sprite(tx), 0, 0);
         this.sonidoHerido = soundChoque;
-        this.Disparo = new controlarDisparos(txBala, soundBala);
+        this.setDisparo(new controlarDisparos(txBala, soundBala));
+        this.tipoDisparo = new DisparoNormal();
         getSprite().setPosition(x, y);
         getSprite().setBounds(x, y, 45, 45);
     }
-
+    
     @Override
     public void mover() {
         if (!herido) {
@@ -55,7 +60,17 @@ public class Nave4 extends TipoObjeto implements movible{
     	SistemaPuntosVidas puntuacion = SistemaPuntosVidas.getInstance();
     	
         if (!herido && otro.getArea().overlaps(getArea())) {
-            herido = true;
+            if (otro instanceof PowerUp) {
+            	PowerUp powerUp = (PowerUp) otro;
+            	if (powerUp.isActivo()) {
+                    setTipoDisparo(new DisparoDoble());
+                    tiempoPowerUp = duracionMaxima;
+                    powerUp.desactivar();
+                    return false;
+                }
+            }
+        	
+        	herido = true;
             tiempoHerido = tiempoHeridoMax;
             sonidoHerido.play();
             puntuacion.restarVida();
@@ -69,15 +84,17 @@ public class Nave4 extends TipoObjeto implements movible{
 
     public void draw(SpriteBatch batch, PantallaJuego juego) {
         actualizar(batch);
-        disparar(juego);
+        disparar(juego, batch);
     }
 
-    public void disparar(PantallaJuego juego) {
+    public void disparar(PantallaJuego juego, SpriteBatch batch) {
     	if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
-            Bullet bala = Disparo.crearBala(getSprite().getX() + getSprite().getWidth() / 2 - 5,
+            tipoDisparo.disparar(this, juego, batch);
+    		/*
+    		Bullet bala = Disparo.crearBala(getSprite().getX() + getSprite().getWidth() / 2 - 5,
                                                      getSprite().getY() + getSprite().getHeight() - 5);
             juego.agregarBala(bala);
-            
+            */
         }
     }
 
@@ -88,4 +105,16 @@ public class Nave4 extends TipoObjeto implements movible{
     public boolean estaHerido() {
         return herido;
     }
+    
+    public void setTipoDisparo(StrategyDisparo tipoDisparo) {
+    	this.tipoDisparo = tipoDisparo;
+    }
+
+	controlarDisparos getDisparo() {
+		return Disparo;
+	}
+
+	public void setDisparo(controlarDisparos disparo) {
+		Disparo = disparo;
+	}
 }
